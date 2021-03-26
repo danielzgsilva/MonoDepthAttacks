e8898b426cb4a72eb7fcce670c1de69b2ce95091
Daniel Silva <danielzgsilva@knights.ucf.edu>
2021-03-26 13:19:28 -0400

    supervised unet with skip conection training on kiii/nyu

---
+++ FCRN/README.md
@@ -0,0 +1,64 @@
+# FCRN implemented in Pytorch 0.4.1
+
+
+### Introduction
+This is a PyTorch(0.4.1) implementation of [Deeper Depth Prediction with Fully Convolutional Residual Networks](http://ieeexplore.ieee.org/document/7785097/). It
+can use Fully Convolutional Residual Networks to realize monocular depth prediction. Currently, we can train FCRN
+using NYUDepthv2 and Kitti Odometry Dataset.
+
+
+### Result
+
+#### NYU Depthv2
+
+The code was tested with Python 3.5 with Pytorch 0.4.1 in 12GB TITAN X.  We train 60 epochs with batch size = 16. The trained model can be download from [BaiduYun](https://pan.baidu.com/s/1A3lq0ntPKBOH-En818bo8A).
+
+ Method |   rml  | rmse  | log10 | Delta1 | Delta2 | Delta3 
+ :-------| :------: | :------: | :------: | :------: | :------: | :------: 
+ FCRN   | 0.127  | 0.573 | 0.055 | 0.811 | 0.953 | 0.988
+ FCRN_ours  | 0.149 | 0.527 | 0.062 | 0.805 | 0.954 | 0.987
+ 
+![Image text](https://github.com/dontLoveBugs/FCRN_pytorch/blob/master/result/nyu.png)
+
+#### Kitti Odometry
+ Method |   rml  | rmse  | log10 | Delta1 | Delta2 | Delta3 
+ :-------| :------: | :------: | :------: | :------: | :------: | :------: 
+ FCRN_ours  | 0.113 | 4.801 | 0.048 | 0.865 | 0.957 | 0.984
+ 
+ ![Image text](https://github.com/dontLoveBugs/FCRN_pytorch/blob/master/result/kitti.png)
+
+### Installation
+The code was tested with Python 3.5 with Pytorch 0.4.1 in 2 GPU TITAN X. 
+
+0. Clone the repo:
+    ```Shell
+    git clone git@github.com:dontLoveBugs/FCRN_pyotrch.git
+    cd FCRN_pytorch
+    ```
+
+1. Install dependencies:
+
+    For PyTorch dependency, see [pytorch.org](https://pytorch.org/) for more details.
+
+    For custom dependencies:
+    ```Shell
+    pip install matplotlib pillow tensorboardX
+    ```
+
+2. Configure your dataset path in "dataloaders/path.py".
+
+3. Training
+
+    To train NYU Depth v2, please do:
+    ```Shell
+    python main.py --dataset nyu
+    ```
+
+    To train it on KITTI, please do:
+    ```Shell
+    python main.py --dataset kitti
+    ```
+
+
+
+
---
+++ FCRN/utils.py
@@ -0,0 +1,115 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2018/10/21 20:57
+# @Author  : Wang Xin
+# @Email   : wangxin_buaa@163.com
+import glob
+import os
+import torch
+import shutil
+import numpy as np
+import matplotlib.pyplot as plt
+from PIL import Image
+
+cmap = plt.cm.jet
+
+
+def parse_command():
+    modality_names = ['rgb', 'rgbd', 'd']
+
+    import argparse
+    parser = argparse.ArgumentParser(description='FCRN')
+    parser.add_argument('--decoder', default='upproj', type=str)
+    parser.add_argument('--resume',
+                        default=None,
+                        type=str, metavar='PATH',
+                        help='path to latest checkpoint (default: ./run/run_1/checkpoint-5.pth.tar)')
+    parser.add_argument('-b', '--batch-size', default=16, type=int, help='mini-batch size (default: 4)')
+    parser.add_argument('--epochs', default=200, type=int, metavar='N',
+                        help='number of total epochs to run (default: 15)')
+    parser.add_argument('--lr', '--learning-rate', default=0.01, type=float,
+                        metavar='LR', help='initial learning rate (default 0.0001)')
+    parser.add_argument('--lr_patience', default=2, type=int, help='Patience of LR scheduler. '
+                                                                   'See documentation of ReduceLROnPlateau.')
+    parser.add_argument('--momentum', default=0.9, type=float, metavar='M',
+                        help='momentum')
+    parser.add_argument('--weight_decay', '--wd', default=0.0005, type=float,
+                        metavar='W', help='weight decay (default: 1e-4)')
+    parser.add_argument('-j', '--workers', default=0, type=int, metavar='N',
+                        help='number of data loading workers (default: 10)')
+    parser.add_argument('--dataset', type=str, default="nyu")
+    parser.add_argument('--manual_seed', default=1, type=int, help='Manually set random seed')
+    parser.add_argument('--print-freq', '-p', default=10, type=int,
+                        metavar='N', help='print frequency (default: 10)')
+    args = parser.parse_args()
+    return args
+
+
+def get_output_directory(args):
+    if args.resume:
+        return os.path.dirname(args.resume)
+    else:
+        save_dir_root = os.path.join(os.path.dirname(os.path.abspath(__file__)))
+        save_dir_root = os.path.join(save_dir_root, 'result', args.decoder)
+        runs = sorted(glob.glob(os.path.join(save_dir_root, 'run_*')))
+        run_id = int(runs[-1].split('_')[-1]) + 1 if runs else 0
+
+        save_dir = os.path.join(save_dir_root, 'run_' + str(run_id))
+        return save_dir
+
+
+# 保存检查点
+def save_checkpoint(state, is_best, epoch, output_directory):
+    checkpoint_filename = os.path.join(output_directory, 'checkpoint-' + str(epoch) + '.pth.tar')
+    torch.save(state, checkpoint_filename)
+    if is_best:
+        best_filename = os.path.join(output_directory, 'model_best.pth.tar')
+        shutil.copyfile(checkpoint_filename, best_filename)
+
+
+def colored_depthmap(depth, d_min=None, d_max=None):
+    if d_min is None:
+        d_min = np.min(depth)
+    if d_max is None:
+        d_max = np.max(depth)
+    depth_relative = (depth - d_min) / (d_max - d_min)
+    return 255 * cmap(depth_relative)[:, :, :3]  # H, W, C
+
+
+def merge_into_row(input, depth_target, depth_pred):
+    rgb = 255 * np.transpose(np.squeeze(input.cpu().numpy()), (1, 2, 0))  # H, W, C
+    depth_target_cpu = np.squeeze(depth_target.cpu().numpy())
+    depth_pred_cpu = np.squeeze(depth_pred.data.cpu().numpy())
+
+    d_min = min(np.min(depth_target_cpu), np.min(depth_pred_cpu))
+    d_max = max(np.max(depth_target_cpu), np.max(depth_pred_cpu))
+    depth_target_col = colored_depthmap(depth_target_cpu, d_min, d_max)
+    depth_pred_col = colored_depthmap(depth_pred_cpu, d_min, d_max)
+    img_merge = np.hstack([rgb, depth_target_col, depth_pred_col])
+
+    return img_merge
+
+
+def merge_into_row_with_gt(input, depth_input, depth_target, depth_pred):
+    rgb = 255 * np.transpose(np.squeeze(input.cpu().numpy()), (1, 2, 0))  # H, W, C
+    depth_input_cpu = np.squeeze(depth_input.cpu().numpy())
+    depth_target_cpu = np.squeeze(depth_target.cpu().numpy())
+    depth_pred_cpu = np.squeeze(depth_pred.data.cpu().numpy())
+
+    d_min = min(np.min(depth_input_cpu), np.min(depth_target_cpu), np.min(depth_pred_cpu))
+    d_max = max(np.max(depth_input_cpu), np.max(depth_target_cpu), np.max(depth_pred_cpu))
+    depth_input_col = colored_depthmap(depth_input_cpu, d_min, d_max)
+    depth_target_col = colored_depthmap(depth_target_cpu, d_min, d_max)
+    depth_pred_col = colored_depthmap(depth_pred_cpu, d_min, d_max)
+
+    img_merge = np.hstack([rgb, depth_input_col, depth_target_col, depth_pred_col])
+
+    return img_merge
+
+
+def add_row(img_merge, row):
+    return np.vstack([img_merge, row])
+
+
+def save_image(img_merge, filename):
+    img_merge = Image.fromarray(img_merge.astype('uint8'))
+    img_merge.save(filename)
