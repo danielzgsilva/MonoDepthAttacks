ccf9df6387ceb02916ed4d4b52827c1f95f00445
Daniel Silva <danielzgsilva@knights.ucf.edu>
2021-03-27 16:11:27 -0400

    kitti transforms

--- dataloaders/kitti_dataloader.py
+++ dataloaders/kitti_dataloader.py
@@ -1,6 +1,8 @@
 import numpy as np
-import dataloaders.transforms as transforms
+# import dataloaders.transforms as transforms
 from dataloaders.dataloader import MyDataloader
+
+from torchvision import transforms
 
 
 class KITTIDataset(MyDataloader):
@@ -9,38 +11,28 @@ class KITTIDataset(MyDataloader):
         self.output_size = (228, 912)
 
     def train_transform(self, rgb, depth):
-        s = np.random.uniform(1.0, 1.5)  # random scaling
-        depth_np = depth / s
-        angle = np.random.uniform(-5.0, 5.0)  # random rotation degrees
-        do_flip = np.random.uniform(0.0, 1.0) < 0.5  # random horizontal flip
-
-        # perform 1st step of data augmentation
         transform = transforms.Compose([
-            transforms.Crop(130, 10, 240, 1200),
-            transforms.Rotate(angle),
-            transforms.Resize(s),
+            transforms.ToPILImage(),
+            transforms.RandomRotation(degrees=5),
             transforms.CenterCrop(self.output_size),
-            transforms.HorizontalFlip(do_flip)
+            transforms.RandomHorizontalFlip(p=0.5),
+            # transforms.ColorJitter(),
+            transforms.ToTensor()
         ])
-        rgb_np = transform(rgb)
-        rgb_np = self.color_jitter(rgb_np)  # random color jittering
-        rgb_np = np.asfarray(rgb_np, dtype='float') / 255
-        # Scipy affine_transform produced RuntimeError when the depth map was
-        # given as a 'numpy.ndarray'
-        depth_np = np.asfarray(depth_np, dtype='float32')
-        depth_np = transform(depth_np)
 
-        return rgb_np, depth_np
+        rgb_tensor = transform(rgb)
+        depth_tensor = transform(depth)
+
+        return rgb_tensor, depth_tensor
 
     def val_transform(self, rgb, depth):
-        depth_np = depth
         transform = transforms.Compose([
-            transforms.Crop(130, 10, 240, 1200),
+            transforms.ToPILImage(),
             transforms.CenterCrop(self.output_size),
+            transforms.ToTensor()
         ])
-        rgb_np = transform(rgb)
-        rgb_np = np.asfarray(rgb_np, dtype='float') / 255
-        depth_np = np.asfarray(depth_np, dtype='float32')
-        depth_np = transform(depth_np)
 
-        return rgb_np, depth_np
+        rgb_tensor = transform(rgb)
+        depth_tensor = transform(depth)
+
+        return rgb_tensor, depth_tensor
