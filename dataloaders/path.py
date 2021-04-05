3c97a960857dacbc5c124983d268d20e8726704d
danielzgsilva <danielzgsilva@knights.ucf.edu>
2021-04-04 22:08:36 -0400

    pathing

--- dataloaders/path.py
+++ dataloaders/path.py
@@ -10,9 +10,9 @@ class Path(object):
     @staticmethod
     def db_root_dir(database):
         if database == 'nyu':
-            return '/home/cap6412.student27/Phase3/data/nyu_depth_v2'
+            return '/home/dsilva/MonoDepthAttacks/data/nyu_depth_v2'
         elif database == 'kitti':
-            return '/home/cap6412.student27/Phase3/data/kitti'
+            return '/home/dsilva/MonoDepthAttacks/data/kitti'
         else:
             print('Database {} not available.'.format(database))
             raise NotImplementedError
